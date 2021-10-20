48fec2c498650afd9c9e0a141be6373bf719a126
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2021-10-20 18:09:16 +0800

    Completed frontend with react

---
+++ my-blog/src/pages/About.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div className="mb-20">
+			<h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+				About
+			</h1>
+			<p className="mx-auto leading-relaxed text-base mb-4">
+				Welcome to my blog! Lorem, ipsum dolor sit amet consectetur adipisicing
+				elit. Porro ullam rem provident quisquam sunt mollitia? Enim facere
+				repudiandae sint obcaecati minus doloremque eveniet nam voluptatum.
+				Obcaecati incidunt harum illum perferendis accusamus sapiente molestias
+				similique odio assumenda, ducimus distinctio maiores facilis adipisci,
+				at pariatur dignissimos autem, eum amet cum. Dolorum eius labore
+				voluptate quo culpa, quia iusto repellat hic sapiente dolores harum,
+				incidunt veniam dolorem qui et voluptatum rem obcaecati, aliquam
+				cupiditate sed numquam. Quod ab quia illum architecto quo dolores
+				laborum mollitia, deleniti quasi reiciendis maiores, sapiente asperiores
+				soluta accusamus magnam eveniet exercitationem? Laborum distinctio,
+				dignissimos, veniam quia laudantium dolorem quibusdam quidem velit in
+				minus voluptatem facere? Iure deleniti suscipit odit minus blanditiis
+				provident cupiditate. Corrupti nam, eum sit necessitatibus aliquid earum
+				cum quam porro accusamus odio sequi culpa velit excepturi quisquam,
+				assumenda, eos voluptatibus non iure odit est at? Inventore eaque
+				tenetur unde, culpa magnam iusto dolore similique optio.
+			</p>
+			<p className="mx-auto leading-relaxed text-base mb-4">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati illum
+				fugit fugiat accusantium reiciendis dicta! Quibusdam neque provident
+				soluta temporibus! Atque cupiditate mollitia eaque. Ullam repellendus
+				tempore ducimus incidunt distinctio debitis, necessitatibus deserunt
+				quos magnam id velit, dolore accusamus esse a saepe veniam mollitia cum
+				autem optio! Dolores, maxime ullam?
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+				cupiditate doloremque beatae maxime explicabo ratione at porro ex
+				distinctio, nobis quia repellendus numquam quos? Laudantium
+				necessitatibus deleniti consequuntur vel, recusandae distinctio nemo qui
+				sit optio magni amet quis tenetur earum velit tempora non quia aliquid
+				fuga ipsum? Totam reprehenderit inventore dicta dolorum nihil, natus
+				voluptas iure accusamus expedita eos laboriosam error blanditiis cum ad
+				molestiae repudiandae animi eveniet. Ad tempora omnis sed nisi aperiam
+				nobis voluptas maxime nostrum repudiandae eius, similique ut nesciunt
+				voluptatibus iste cum pariatur quidem dolores aliquid dicta quae vero
+				itaque at. Veritatis a repudiandae sequi tempore?
+			</p>
+		</div>
+	);
+};
+
+export default About;
